842932a9b6128156631f2af4f2515a63b3ec2f26
neelavroshafin <shafinneelavro@gmail.com>
2026-05-12 22:30:30 +0600

    ⏺ feat: add HRM module and migrate auth to academic service with FBAC role-based access control

--- src/main/java/com/example/academic_service/config/SecurityConfig.java
+++ src/main/java/com/example/academic_service/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5000","http://167.172.86.59:5000")); // Your Flutter web port
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5000","http://167.172.86.59:5000","http://129.212.238.237:5000/")); // Your Flutter web port
         configuration.setAllowedMethods(Arrays.asList(
                 "GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH" // ← add PATCH
         ));
